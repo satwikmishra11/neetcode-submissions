1a45fe1cdd5733890174dffebbe851dd022c4f1c
Satwik Mishra <150122188+satwikmishra11@users.noreply.github.com>
2026-09-22 12:14:18 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        result = [0] * n
+        stack = []  # Store indices
+        
+        for i in range(n):
+            # While stack is not empty and current temp is warmer than stack top
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                result[prev_index] = i - prev_index
+            stack.append(i)
+        
+        return result
+            
